99efcf17873315938228e26eac35f60d7fc8c896
Sichan Yoo <chanyoo@amazon.com>
2024-01-30 10:06:47 -0800

    Add setters to HttpResponse & make code that mutates HttpResponse in CRTClientEngine async using Task.

--- Sources/ClientRuntime/Networking/Http/HttpResponse.swift
+++ Sources/ClientRuntime/Networking/Http/HttpResponse.swift
@@ -10,6 +10,18 @@ public actor HttpResponse: HttpUrlResponse {
     public var body: ByteStream
     public var statusCode: HttpStatusCode
 
+    public func addHeaders(additionalHeaders: Headers) {
+        self.headers.addAll(headers: additionalHeaders)
+    }
+
+    public func setBody(newBody: ByteStream) {
+        self.body = newBody
+    }
+
+    public func setStatusCode(newStatusCode: HttpStatusCode) {
+        self.statusCode = newStatusCode
+    }
+
     public init(headers: Headers = .init(), statusCode: HttpStatusCode = .processing, body: ByteStream = .noStream) {
         self.headers = headers
         self.statusCode = statusCode
